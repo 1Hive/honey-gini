78eac67805e11b910da2eb7ec5808d0e5794141c
Oliver Nordbjerg <hi@notbjerg.me>
2021-06-25 05:02:14 +0200

    Update addresses

--- bin.js
+++ bin.js
@@ -86,13 +86,15 @@ async function fetchHolders (
 
 const BLACKLIST = [
   // Common Pool
-  '0x05e42c4ae51ba28d8acf8c371009ad7138312ca4',
+  '0x4ba7362f9189572cbb1216819a45aba0d0b2d1cb',
   // HNY-WXDAI
   '0x4505b262dc053998c10685dc5f9098af8ae5c8ad',
   // HNY-STAKE
   '0x298c7326a6e4a6108b88520f285c7dc89403347d',
   // WETH-HNY
-  '0x89e2f342b411032a580fefa17f96da6a5bef4112'
+  '0x89e2f342b411032a580fefa17f96da6a5bef4112',
+  // AGVE-HNY
+  '0x50a4867aee9cafd6ddc84de3ce59df027cb29084'
 ]
 const MINIMUM_HOLDINGS = "100000000000000000" // 1 HNY
 
